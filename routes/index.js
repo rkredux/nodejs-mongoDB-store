d00aa1fef3f02e46615b61dd33e60e1c67c94e6c
Rahul <rkredux@gmail.com>
2017-07-27 12:42:47 -0700

    Create routes for edit and update stores

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,11 @@ router.get("/stores", catchErrors(storeController.getStores));
 // route for add store page
 router.get("/add", storeController.addStore); 
 
-router.post("/add", catchErrors(storeController.createStore)); 
+router.get(`/stores/:id/edit`, catchErrors(storeController.editStore)); 
+
+router.post("/add", catchErrors(storeController.createStore));
+router.post(`/add/:id`, catchErrors(storeController.updateStore));
+
 
 module.exports = router; 
 
