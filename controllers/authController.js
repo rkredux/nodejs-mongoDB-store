2f2c50c27cf552e529fcd678cdde447465a55fad
Rahul <rkredux@gmail.com>
2017-08-06 10:17:42 -0400

    Create logout and isLoggedIn methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,21 @@ exports.login = passport.authenticate("local", {
 	successRedirect: "/", 
 	successFlash: "You are now logged in"
 }); 
+
+
+exports.logout = (req, res) =>{
+	req.logout(); 
+	req.flash("success", "You are now logged out!"); 
+	res.redirect("/"); 	
+}; 
+
+
+// Oh this is a middleware so we need to use next in this
+exports.isLoggedIn = (req, res, next) =>{
+	//first check if the user is authenticated
+	if (req.isAuthenticated()) {
+		return next(); 
+	}
+	req.flash("error", "Oops, you must be logged in!");
+	res.redirect("/login");  
+}; 
